055daeb901b5e118b22b127290be0d0bf6dca63a
wakil bouharrat <ab2251@rit.edu>
2021-09-20 21:50:23 +0400

    turtle code p2

---
+++ Abdelouakil_Bouharrat_lab2_2.py
@@ -0,0 +1,13 @@
+import turtle
+def turtle_code():
+    turtle.forward(100)
+    turtle.left(87)
+    turtle.setheading(127)
+    turtle.up()
+    turtle.goto(50,40)
+    turtle.down()
+    turtle.home()
+    turtle.circle(25)
+def main():
+    turtle_code()
+main()    
